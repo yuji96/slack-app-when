213793403b8b745b22e699ad749806afe6c11217
yuji96 <y.yama683@gmail.com>
2021-07-05 15:15:26 +0900

    add example modal

--- src/app.py
+++ src/app.py
@@ -5,26 +5,33 @@ from os.path import join, dirname
 
 import coloredlogs
 from dotenv import load_dotenv
-from slack_bolt import App
+from slack_bolt import App, Ack
+from slack_sdk import WebClient
 
 
+load_dotenv(join(dirname(__file__), '../.env'))
+logger = getLogger(__name__)
+logger.setLevel("INFO")
+coloredlogs.install(level="INFO", logger=logger)
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+    logger=logger,
+)
 
-def read_json(path, logger):
+
+def read_json(path):
     abs_path = join(dirname(__file__), path)
     with open(abs_path) as f:
         out = json.load(f)
     return out
 
 
-load_dotenv(join(dirname(__file__), '../.env'))
-logger = getLogger(__name__)
-logger.setLevel("DEBUG")
-coloredlogs.install(level="DEBUG", logger=logger)
-app = App(
-    token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
-    logger=logger,
-)
+@app.shortcut("test")
+def open_modal_test(ack: Ack, body: dict, client: WebClient):
+    ack()
+    client.views_open(trigger_id=body["trigger_id"],
+                      view=read_json("blocks/modal.json"))
 
 
 @app.event("app_home_opened")
@@ -35,11 +42,12 @@ def update_home_tab(client, event, logger):
             view={
                 "type": "home",
                 "callback_id": "home_view",
-                "blocks": read_json("blocks/home.json", logger)
+                "blocks": read_json("blocks/home.json")
             }
         )
     except Exception as e:
         logger.error(f"Error publishing home tab: {e}")
 
 
-app.start(port=int(os.environ.get("PORT", 3000)))
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
