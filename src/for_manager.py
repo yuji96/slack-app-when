bbeed0a14238d14728e56ee4ac862c224bc86e6f
cn-eveer <sienreeve46@gmail.com>
2021-07-10 06:50:00 +0900

    add target modals

--- src/for_manager.py
+++ src/for_manager.py
@@ -22,27 +22,53 @@ def home_tab(client, event, logger):
         }
     )
 
+def set_target(ack: Ack, body: dict, client: WebClient):
 
-def set_schedule(ack: Ack, body: dict, client: WebClient):
-    """日程調整用 Modal を表示する．"""
     ack()
-    view_json = read_json("./modals/set_schedule.json")
-    today = datetime.today()
-    tomorrow = today + timedelta(1)
-
-    # 開始日を「今日」に、終了日を「明日」に設定
-    view_json["blocks"][1]['accessory']['initial_date'] = str(today.date())
-    view_json["blocks"][2]['accessory']['initial_date'] = str(tomorrow.date())
+    view_json = read_json("./modals/set_target.json")
 
     client.views_open(trigger_id=body["trigger_id"],
                       view=view_json)
 
+def check_target(ack: Ack, body: dict, client: WebClient, view: dict):
+
+    ack()
+    block_id = body["view"]["blocks"][0]["block_id"]
+    target_value = view["state"]["values"][block_id]["target-select"]["selected_option"]["value"]
+
+    set_schedule(body["trigger_id"],client,target_value)
+
+def set_schedule(trigger_id, client: WebClient, target):
+    """日程調整用 Modal を表示する．"""
+    read_file = "./modals/set_schedule-"+target+".json"
+    view_json = read_json(read_file)
+
+    # 開始日を「今日」に、終了日を「明日」に設定
+    today = datetime.today()
+    tomorrow = today + timedelta(1)
+    view_json["blocks"][1]['accessory']['initial_date'] = str(today.date())
+    view_json["blocks"][2]['accessory']['initial_date'] = str(tomorrow.date())
+
+    if "channel" in target:
+        
+        # TODO:OPTION追加用のモーダルを作成
+        options = [ {
+                        'text': {
+                            'type': 'plain_text', 
+                            'text': item["name"], 
+                            'emoji': True
+                        }, 
+                        'value': item["id"]
+                    } for item in client.conversations_list(types=target)['channels']]
+        
+        view_json["blocks"][6]["element"]["options"] = options
+
+    client.views_open(trigger_id=trigger_id,
+                      view=view_json)
 
 def update_schedule(ack: Ack, body: dict, client: WebClient):
     """日程調整用 Modal の内容を更新する．"""
     ack()
-    action_block = body["actions"][0]["block_id"]
-    insert_value = body["view"]["state"]["values"][action_block]
     view_json = read_json("./modals/set_schedule.json")
     view_json["blocks"] = body["view"]["blocks"]
 
@@ -50,44 +76,42 @@ def update_schedule(ack: Ack, body: dict, client: WebClient):
                         hash=body["view"]["hash"],
                         view_id=body["view"]["id"])
 
-
-def send_message(ack: Ack, body: dict, client: WebClient):
+def check_users(ack: Ack, body: dict, client: WebClient, view: dict):
     """日程調整用 Modal の提出を処理する．"""
     ack()
-    users_list_block = body["view"]["blocks"][7]["block_id"]
-    users_list = body["view"]["state"]["values"][users_list_block]['multi_conversations_select-action']['selected_conversations']
+    block_id = body["view"]["blocks"][6]["block_id"]
+    users_list = view["state"]["values"][block_id]["multi_users_select-action"]["selected_users"]
 
-    if users_list:
-        # 選択したユーザ・チャンネルにメッセージを投稿する
-        for item in users_list:
-            client.chat_postMessage(channel=item,
-                                    text="This is a test",
-                                    blocks=read_json("./statics/hello.json"))
-    else:
+    send_message(users_list,client)
 
-        # TODO:　モーダルにて選択のValidationを設定する必要がある
+def check_channels(ack: Ack, body: dict, client: WebClient, view: dict):
 
-        """
-        view_json = read_json("./modals/set_schedule.json")
-        view_json["blocks"] = body["view"]["blocks"]
-        client.view(view=view_json,
-                            hash=body["view"]["hash"],
-                            view_id=body["view"]["id"])
-        """
-        pass
+    ack()
+    block_id = body["view"]["blocks"][6]["block_id"]
+    channel = [list(view["state"]["values"][block_id].values())[0]['selected_option']['value']]
 
+    send_message(channel,client)
+
+def send_message(lists: list, client: WebClient):
+
+    # 選択したユーザ・チャンネルにメッセージを投稿する
+    for item in lists:
+
+        client.chat_postMessage(channel=item,
+                                text="Please Check the message",
+                                blocks=read_json("./statics/hello.json"))
 
 def register(app):
     logger.info("register")
-    
+
     # TODO: `Calleback ID` の命名規則を統一したい．
 
     # アプリのタブ　イベント
     app.event("app_home_opened")(home_tab)
 
     # モーダル発動　イベント
-    app.shortcut("set_schedules")(set_schedule)
-    app.action("set-schedules")(set_schedule)
+    app.action("schedule-request")(set_target)
+    app.shortcut("schedule-request")(set_target)
 
     # モーダル入力時
     app.action("datepicker-action")(update_schedule)
@@ -95,4 +119,6 @@ def register(app):
     app.action("result-option")(update_schedule)
 
     # モーダル提出時
-    app.view("set-schedules")(send_message)
+    app.view("schedule-request")(check_target)
+    app.view("set-schedules-private-DM")(check_users)
+    app.view("set-schedules-public-channel")(check_channels)
