ef51e922dbb862b899ed603c0d4c94ee505baee4
yuji96 <y.yama683@gmail.com>
2021-07-09 15:46:38 +0900

    fix coding style

--- src/for_manager.py
+++ src/for_manager.py
@@ -1,7 +1,7 @@
 from slack_bolt import Ack
 from slack_sdk import WebClient
 
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 from blocks import read_json
 from settings import set_logger
@@ -10,39 +10,36 @@ from settings import set_logger
 logger = set_logger(__name__)
 
 
-# TODO: チュートリアルの関数なので将来的に削除する
 def home_tab(client, event, logger):
-    logger.info("open")
+    """アプリホームビューを編集する．"""
     blocks = read_json("./statics/home.json")
-    try:
-        client.views_publish(
-            user_id=event["user"],
-            view={
-                "type": "home",
-                "callback_id": "home_view",
-                "blocks": blocks
-            }
-        )
-    except Exception as e:
-        logger.error(f"Error publishing home tab: {e}")
+    client.views_publish(
+        user_id=event["user"],
+        view={
+            "type": "home",
+            "callback_id": "home_view",
+            "blocks": blocks
+        }
+    )
 
-# Modalの新規作成時
+
 def set_schedule(ack: Ack, body: dict, client: WebClient):
+    """日程調整用 Modal を表示する．"""
     ack()
     view_json = read_json("./modals/set_schedule.json")
     today = datetime.today()
     tomorrow = today + timedelta(1)
 
-    #　開始日を「今日」に、終了日を「明日」に設定
-    view_json["blocks"][1]['accessory']['initial_date']=str(today.date())
-    view_json["blocks"][2]['accessory']['initial_date']=str(tomorrow.date())
+    # 開始日を「今日」に、終了日を「明日」に設定
+    view_json["blocks"][1]['accessory']['initial_date'] = str(today.date())
+    view_json["blocks"][2]['accessory']['initial_date'] = str(tomorrow.date())
 
     client.views_open(trigger_id=body["trigger_id"],
                       view=view_json)
-
-# Modalの更新時
+
+
 def update_schedule(ack: Ack, body: dict, client: WebClient):
- 
+    """日程調整用 Modal の内容を更新する．"""
     ack()
     action_block = body["actions"][0]["block_id"]
     insert_value = body["view"]["state"]["values"][action_block]
@@ -53,24 +50,22 @@ def update_schedule(ack: Ack, body: dict, client: WebClient):
                         hash=body["view"]["hash"],
                         view_id=body["view"]["id"])
 
-# Modalの提出時
-def send_message(ack: Ack, body: dict, client: WebClient):
 
+def send_message(ack: Ack, body: dict, client: WebClient):
+    """日程調整用 Modal の提出を処理する．"""
     ack()
     users_list_block = body["view"]["blocks"][7]["block_id"]
     users_list = body["view"]["state"]["values"][users_list_block]['multi_conversations_select-action']['selected_conversations']
 
-    if users_list != []:
-        
+    if users_list:
         # 選択したユーザ・チャンネルにメッセージを投稿する
         for item in users_list:
             client.chat_postMessage(channel=item,
                                     text="This is a test",
                                     blocks=read_json("./statics/hello.json"))
-
     else:
 
-        # TODO:　モーダルにて選択のValidationを設定する必要がある    
+        # TODO:　モーダルにて選択のValidationを設定する必要がある
 
         """
         view_json = read_json("./modals/set_schedule.json")
@@ -81,8 +76,11 @@ def send_message(ack: Ack, body: dict, client: WebClient):
         """
         pass
 
+
 def register(app):
     logger.info("register")
+    
+    # TODO: `Calleback ID` の命名規則を統一したい．
 
     # アプリのタブ　イベント
     app.event("app_home_opened")(home_tab)
