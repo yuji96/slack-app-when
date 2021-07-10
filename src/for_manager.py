f45640b2790cc5da93eef4535ad43754ce99a942
cn-eveer <sienreeve46@gmail.com>
2021-07-10 15:58:23 +0900

    update

--- src/for_manager.py
+++ src/for_manager.py
@@ -12,37 +12,23 @@ logger = set_logger(__name__)
 
 def home_tab(client, event, logger):
     """アプリホームビューを編集する．"""
-    blocks = read_json("./statics/home.json")
-    client.views_publish(
-        user_id=event["user"],
-        view={
-            "type": "home",
-            "callback_id": "home_view",
-            "blocks": blocks
-        }
-    )
 
+    view_json = read_json("./statics/home.json")
+    client.views_publish(user_id=event["user"],
+                         view=view_json)
 
-def set_target(ack: Ack, body: dict, client: WebClient):
-
-    ack()
-    view_json = read_json("./modals/set_target.json")
-
-    client.views_open(trigger_id=body["trigger_id"],
-                      view=view_json)
-
-
-def check_target(ack: Ack, body: dict, client: WebClient, view: dict):
-
-    ack()
-    target_value = view["state"]["values"]["target"]["target-select"]["selected_option"]["value"]
-
-    set_schedule(body["trigger_id"], client, target_value)
-
-
-def set_schedule(trigger_id, client: WebClient, target):
+def set_schedule(ack: Ack, body: dict, client: WebClient, view: dict):
     """日程調整用 Modal を表示する．"""
-    read_file = "./modals/set_schedule-"+target+".json"
+
+    # アプリのホームタブから
+    if "actions" in body:
+        target = body["actions"][0]["value"]
+
+    # ショートカットから
+    else:
+        target = body["callback_id"].removeprefix("set_schedules_")
+
+    read_file = "./modals/set_schedules-" + target + ".json"
     view_json = read_json(read_file)
 
     # 開始日を「今日」に、終了日を「明日」に設定
@@ -51,16 +37,18 @@ def set_schedule(trigger_id, client: WebClient, target):
     view_json["blocks"][1]['accessory']['initial_date'] = str(today.date())
     view_json["blocks"][2]['accessory']['initial_date'] = str(tomorrow.date())
 
-    client.views_open(trigger_id=trigger_id,
+    ack()
+    client.views_open(trigger_id=body["trigger_id"],
                       view=view_json)
 
 
 def update_schedule(ack: Ack, body: dict, client: WebClient):
     """日程調整用 Modal の内容を更新する．"""
-    ack()
+
     view_json = read_json("./modals/set_schedule.json")
     view_json["blocks"] = body["view"]["blocks"]
 
+    ack()
     client.views_update(view=view_json,
                         hash=body["view"]["hash"],
                         view_id=body["view"]["id"])
@@ -68,19 +56,18 @@ def update_schedule(ack: Ack, body: dict, client: WebClient):
 
 def check_users(ack: Ack, body: dict, client: WebClient, view: dict):
     """日程調整用 Modal の提出を処理する．"""
-    ack()
-    block_id = body["view"]["blocks"][6]["block_id"]
-    users_list = view["state"]["values"][block_id]["multi_users_select-action"]["selected_users"]
 
+    users_list = view["state"]["values"]["users_select"]["multi_users_select-action"]["selected_users"]
+
+    ack()
     send_message(users_list, client)
 
 
 def check_channels(ack: Ack, body: dict, client: WebClient, view: dict):
 
-    ack()
-    block_id = body["view"]["blocks"][6]["block_id"]
-    channel = [list(view["state"]["values"][block_id].values())[0]["selected_channel"]]
+    channel = [list(view["state"]["values"]["channel_select"].values())[0]["selected_channel"]]
 
+    ack()
     send_message(channel, client)
 
 
@@ -100,9 +87,14 @@ def register(app):
     # アプリのタブ　イベント
     app.event("app_home_opened")(home_tab)
 
-    # モーダル発動　イベント
-    app.action("schedule-request")(set_target)
-    app.shortcut("schedule-request")(set_target)
+    # アプリのタブ内 モーダル発動　イベント
+    app.action("set_schedules_channel")(set_schedule)
+    app.action("set_schedules_im")(set_schedule)
+
+    # ショートカット  モーダル発動　イベント
+    app.shortcut("set_schedules_channel")(set_schedule)
+    app.shortcut("set_schedules_im")(set_schedule)
+
 
     # モーダル入力時
     app.action("datepicker-action")(update_schedule)
@@ -110,6 +102,5 @@ def register(app):
     app.action("result-option")(update_schedule)
 
     # モーダル提出時
-    app.view("schedule-request")(check_target)
-    app.view("set-schedules-private-DM")(check_users)
-    app.view("set-schedules-public-channel")(check_channels)
+    app.view("set_schedules-im")(check_users)
+    app.view("set_schedules-channel")(check_channels)
