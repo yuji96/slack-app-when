4d2bbe43f7319812320af1365f1db4cc0b98250b
yuji96 <y.yama683@gmail.com>
2021-07-17 12:38:33 +0900

    fix coding style

--- src/for_manager.py
+++ src/for_manager.py
@@ -17,90 +17,44 @@ logger = set_logger(__name__)
 
 def home_tab(client: WebClient, event: dict) -> dict:
     """アプリホームビューを編集する．"""
-
-    # ホームメニューのブロック
-    view_json = read_json("./app_tab/home.json")
-    
-    # ホームメニューを表示する
-    client.views_publish(
-        user_id = event["user"],
-        view = view_json)
+    client.views_publish(user_id=event["user"],
+                         view=read_json("./app_tab/home.json"))
 
 
 def open_modal(ack: Ack, body: dict, client: WebClient, view: dict) -> dict:
     """日程調整用 Modal を表示する．"""
-
-    # モーダルの基礎ブロック
     view_json = read_json("./modals/set_schedules.json")
 
-    # アプリホームビュー から
     if "actions" in body:
-        target_id = body["actions"][0]["action_id"]
-        target = body["actions"][0]["value"]
-
-    # ショートカット から
+        # アプリホームビュー から
+        target_id = str(body["actions"][0]["action_id"])
     else:
-        target_id = body["callback_id"]
-        target = target_id.removeprefix("set_schedules-")
-
+        # ショートカット から
+        target_id = str(body["callback_id"])
     view_json["callback_id"] = target_id
+    target = target_id.removeprefix("set_schedules-")
 
-    #　モーダルのブロック を追加
-    view_json["blocks"] = insert_block(target)    
+    # モーダルのブロック を追加
+    view_json["blocks"] = build_schedule_block(target)
 
     ack()
-    
+
     # モーダルを開く
     client.views_open(
         trigger_id=body["trigger_id"],
         view=view_json)
 
 
-def insert_block(target: str) -> list:
-    """日程調整用 Modal のブロックを追加する．"""
-
-    insert_blocks = []
-    
-    # 日程調整用 Modal ブロックのディレクトリ
-    directory = "./set"
-
-    # 日程選択 のブロック
-    today = datetime.today()
-    tomorrow = today + timedelta(days=1)
-    date_json=read_json(f"{directory}/set_date.json")
-
-    # 開始日 ->「今日」、終了日 ->「明日」 に設定
-    date_json[1]['accessory']['initial_date'] = str(today.date())
-    date_json[2]['accessory']['initial_date'] = str(tomorrow.date())
-
-    insert_blocks.extend(date_json)
-
-    # 時間選択 のブロック
-    time_json = read_json(f"{directory}/set_time.json")
-    insert_blocks.extend(time_json)
-
-    # 回答者選択 のブロック
-    target_json = read_json(f"{directory}/set_{target}.json")
-    insert_blocks.extend(target_json)
-
-    # チャンネル用の共有設定 のブロック
-    if target == "channel":
-        display_json = read_json(f"{directory}/set_display.json")
-        insert_blocks.extend(display_json)
-
-    return insert_blocks
-
-
 def update_modal(ack: Ack, body: dict, client: WebClient):
     """日程調整用 Modal の内容を更新する．"""
 
     view_json = read_json("./modals/set_schedules.json")
-    
+
     # 更新する内容
     view_json["blocks"] = body["view"]["blocks"]
     view_json["callback_id"] = body["view"]["callback_id"]
 
-    #TODO: 入力した時間と日程が有効かどうか確認する
+    # TODO: 入力した時間と日程が有効かどうか確認する
 
     ack()
 
@@ -111,63 +65,20 @@ def update_modal(ack: Ack, body: dict, client: WebClient):
         view_id=body["view"]["id"])
 
 
-def check_users(values: dict) -> list:
-    """日程調整用 Modal の「回答者」の欄の ユーザー を取得する．"""
-
-    return values["users_select"]["multi_users_select-action"]["selected_users"]
-
-
-def check_channels(values: dict) -> list:
-    """日程調整用 Modal の「回答者」の欄の チャンネル を取得する．"""
-
-    return [list(values["channel_select"].values())[0]["selected_channel"]]
-
-
-def get_modal_inputs(body: dict, values: dict) -> dict:
-    """日程調整用 Modal の入力を取得する．"""
-
-    # 日付
-    start_date = values["start_date"]["host_datepicker-action"]["selected_date"]
-    end_date = values["end_date"]["host_datepicker-action"]["selected_date"]
-    
-    # 時間
-    start_time = values["start_time"]["host_timepicker-action"]["selected_time"]
-    end_time = values["end_time"]["host_timepicker-action"]["selected_time"]
-
-    modal_inputs = {
-        "host" : "<@"+body["user"]["id"]+">",
-        "host_id" : body["user"]["id"],
-        "date" : start_date + " から " + end_date,
-        "time" : start_time + " から " + end_time,
-        "start_date" : start_date,
-        "end_date" : end_date,
-        "start_time" : start_time,
-        "end_time" : end_time
-    }
-    
-    # チャンネル用　の回答共有設定
-    if "display_result" in values:
-        setting = values["display_result"]["result-option"]["selected_option"]
-        modal_inputs["setting"] = setting["text"]["text"]
-        modal_inputs["setting_value"] = setting["value"]
-
-    return modal_inputs
-
-
 def check_modal(ack: Ack, body: dict, client: WebClient, view: dict):
     """日程調整用 Modal の提出を確認する．"""
 
     values = view["state"]["values"]
-    
-    #　モーダルの入力値を取得
+
+    # モーダルの入力値を取得
     modal_inputs = get_modal_inputs(body, values)
 
     # チャンネル・個人用 の確認
     target = body["view"]["callback_id"]
     if target == 'set_schedules-im':
-        modal_inputs["send_lists"] = check_users(values)
+        modal_inputs["send_lists"] = get_users(values)
     elif target == 'set_schedules-channel':
-        modal_inputs["send_lists"] = check_channels(values)
+        modal_inputs["send_lists"] = get_channels(values)
 
     ack()
 
@@ -176,7 +87,7 @@ def check_modal(ack: Ack, body: dict, client: WebClient, view: dict):
 
 
 def send_message(inputs: dict, client: WebClient):
-    """選択した回答者宛に メッセージ を送る．"""   
+    """選択した回答者宛に メッセージ を送る．"""
 
     # 回答者宛のメッセージブロック
     message_json = read_json("./message/from_host.json")
@@ -186,17 +97,17 @@ def send_message(inputs: dict, client: WebClient):
             if item["block_id"] not in inputs:
                 message_json.remove(item)
             else:
-                item["text"]["text"]+=inputs[item["block_id"]]
+                item["text"]["text"] += inputs[item["block_id"]]
 
     # 主催者用の　スケジュール調整の詳細ブロック
     detail_json = read_json("./message/schedule_detail.json")
-    detail_json.extend(message_json[3:-1])
-    
+    detail_json.extend(message_json[3:-1])  # TODO: filter section
+
     # 主催者 に作成した調整を送信する
     response = client.chat_postMessage(channel=inputs["host_id"],
-                            text="メッセージを確認してください",
-                            blocks=detail_json,
-                            as_user=True)
+                                       text="メッセージを確認してください",
+                                       blocks=detail_json,
+                                       as_user=True)
 
     # 主催者と主催メッセージの情報を追加する
     message_json[-1]["elements"][0]["value"] = f"{response['channel']}-{inputs['host_id']}-{response['ts']}"
@@ -232,3 +143,85 @@ def register(app):
     app.view("set_schedules-im")(check_modal)
     app.view("set_schedules-channel")(check_modal)
 
+
+##########################################
+# utilities
+##########################################
+
+
+def build_schedule_block(target: str) -> list:
+    """日程調整用 Modal のブロックを追加する．"""
+
+    insert_blocks = []
+
+    # 日程調整用 Modal ブロックのディレクトリ
+    directory = "./set"
+
+    # 日程選択 のブロック
+    today = datetime.today()
+    tomorrow = today + timedelta(days=1)
+    date_json = read_json(f"{directory}/set_date.json")
+
+    # 開始日 ->「今日」、終了日 ->「明日」 に設定
+    date_json[1]['accessory']['initial_date'] = str(today.date())
+    date_json[2]['accessory']['initial_date'] = str(tomorrow.date())
+
+    insert_blocks.extend(date_json)
+
+    # 時間選択 のブロック
+    time_json = read_json(f"{directory}/set_time.json")
+    insert_blocks.extend(time_json)
+
+    # 回答者選択 のブロック
+    target_json = read_json(f"{directory}/set_{target}.json")
+    insert_blocks.extend(target_json)
+
+    # チャンネル用の共有設定 のブロック
+    if target == "channel":
+        display_json = read_json(f"{directory}/set_display.json")
+        insert_blocks.extend(display_json)
+
+    return insert_blocks
+
+
+def get_users(values: dict) -> list:
+    """日程調整用 Modal の「回答者」の欄の ユーザー を取得する．"""
+
+    return values["users_select"]["multi_users_select-action"]["selected_users"]
+
+
+def get_channels(values: dict) -> list:
+    """日程調整用 Modal の「回答者」の欄の チャンネル を取得する．"""
+
+    return [list(values["channel_select"].values())[0]["selected_channel"]]
+
+
+def get_modal_inputs(body: dict, values: dict) -> dict:
+    """日程調整用 Modal の入力を取得する．"""
+
+    # 日付
+    start_date = values["start_date"]["host_datepicker-action"]["selected_date"]
+    end_date = values["end_date"]["host_datepicker-action"]["selected_date"]
+
+    # 時間
+    start_time = values["start_time"]["host_timepicker-action"]["selected_time"]
+    end_time = values["end_time"]["host_timepicker-action"]["selected_time"]
+
+    modal_inputs = {
+        "host": "<@"+body["user"]["id"]+">",
+        "host_id": body["user"]["id"],
+        "date": start_date + " から " + end_date,
+        "time": start_time + " から " + end_time,
+        "start_date": start_date,
+        "end_date": end_date,
+        "start_time": start_time,
+        "end_time": end_time
+    }
+
+    # チャンネル用　の回答共有設定
+    if "display_result" in values:
+        setting = values["display_result"]["result-option"]["selected_option"]
+        modal_inputs["setting"] = setting["text"]["text"]
+        modal_inputs["setting_value"] = setting["value"]
+
+    return modal_inputs
--- src/for_member.py
+++ src/for_member.py
@@ -26,37 +26,36 @@ def open_modal(ack: Ack, body: dict, client: WebClient):
 
     view_json = read_json("./modals/answer_schedule.json")
 
-    #　モーダルのブロック を追加
+    # モーダルのブロック を追加
     view_json["blocks"], time = insert_block(body)
 
     ack()
 
     # モーダルを開く
     client.views_open(
-        trigger_id = body["trigger_id"],
-        view = view_json)
+        trigger_id=body["trigger_id"],
+        view=view_json)
 
 
 def insert_block(body: dict) -> list:
     """回答用 Modal のブロックを追加する．"""
 
-    insert_blocks=[]
+    insert_blocks = []
     values = {}
 
     # [主催者、日、時間、設定] をメッセージから取得する
-    items = [ 'host', 'date', 'time', 'setting' ]
-    for item in body["message"]["blocks"] :
+    items = ['host', 'date', 'time', 'setting']
+    for item in body["message"]["blocks"]:
         if "block_id" in item and item["block_id"] in items:
-            values[ item["block_id"] ] = item["text"]["text"].split("\n")[1]
+            values[item["block_id"]] = item["text"]["text"].split("\n")[1]
 
     # 開催日から終了日の間　の日付を生成する
     dates = values["date"].split(" から ")
-    values["date"] = [ str(item.date()) for 
-        item in list(pd.date_range(dates[0],dates[1])) ]
+    values["date"] = [str(item.date()) for item in pd.date_range(dates[0], dates[1])]
 
     # 時間選択　のブロック
     for item in values["date"]:
-        insert_blocks.extend(generate_block(item,values["time"],1))
+        insert_blocks.extend(generate_block(item, values["time"], 1))
 
     # 主催者宛　のブロック
     users_json = generate_description_block(body["actions"][-1]["value"])
@@ -71,12 +70,15 @@ def generate_block(date: str, time: str, num: int) -> list:
     divider_block = {"type": "divider"}
 
     # Pattern 1
-    pattern = [divider_block, generate_date_block(date,time), 
-        generate_time_block(date,time,num),generate_options_block(date,"None")]
+    pattern = [divider_block,
+               generate_date_block(date, time),
+               generate_time_block(date, time, num),
+               generate_options_block(date, "None")]
 
     # Pattern 2
-    pattern = [divider_block,generate_label_block(date,time),
-        generate_options_block(date)]
+    pattern = [divider_block,
+               generate_label_block(date, time),
+               generate_options_block(date)]
 
     return pattern
 
@@ -86,31 +88,31 @@ def generate_date_block(date: str, time: str) -> dict:
 
     date_block = read_json("./answer/add_date.json")
     date_block["block_id"] = date
-    date_block["text"]["text"] = date_block["text"]["text"].replace("date",date).replace("time",time)
+    date_block["text"]["text"] = date_block["text"]["text"].replace("date", date).replace("time", time)
     date_block["accessory"]["value"] += f"-{date}"
 
     return date_block
 
 
-def generate_time_block(date: str, time: str, num: int)  -> dict:
+def generate_time_block(date: str, time: str, num: int) -> dict:
     """回答用 Modal の時間選択のブロックを追加する．"""
 
-    start_time,end_time =  time.split(" から ")
+    start_time, end_time = time.split(" から ")
 
     time_block = read_json("./answer/add_time.json")
-    time_block["block_id"] = time_block["block_id"].replace("date",date).replace("opt",str(num))
+    time_block["block_id"] = time_block["block_id"].replace("date", date).replace("opt", str(num))
     time_block["elements"][0]["initial_time"] = start_time
     time_block["elements"][1]["initial_time"] = end_time
 
     return time_block
 
 
-def generate_label_block(date = "", time = ""):
+def generate_label_block(date="", time=""):
 
     label_json = read_json("./answer/add_date-text.json")
 
     label_json["block_id"] = date
-    label_json["label"]["text"] = label_json["label"]["text"].replace("date",date).replace("time",time)
+    label_json["label"]["text"] = label_json["label"]["text"].replace("date", date).replace("time", time)
 
     return label_json
 
@@ -124,7 +126,7 @@ def generate_label_element(initial: str):
     return label_json
 
 
-def generate_options_block(date: str, value = "default"):
+def generate_options_block(date: str, value="default"):
 
     option_json = read_json("./answer/add_button_options.json")
     option_json["block_id"] = f"{date}-opt"
@@ -179,53 +181,48 @@ def update_modal(ack: Ack, body: dict, client: WebClient):
         button_select = action.split('-')[-1]
         date = target.removesuffix("-opt")
 
-        for index,item in enumerate(view_json["blocks"]):
+        for index, item in enumerate(view_json["blocks"]):
             if item["block_id"] == target:
-                
+
                 value = view_json["blocks"][index-1]["element"]
-                
+
                 pprint(body)
                 if "initial_value" in value:
                     condition = (
                         (value["initial_value"] == "All" and button_select == "no") or
                         (value["initial_value"] == "None" and button_select == "yes") or
-                        (value["initial_value"] not in ["All","None"]) )
+                        (value["initial_value"] not in ["All", "None"]))
                 else:
                     condition = "initial_value" not in value
                 print(condition)
                 if condition:
 
-                    update_button_json = generate_options_block(date,button_select)
+                    update_button_json = generate_options_block(date, button_select)
                     body["view"]["state"]["values"][date]["plain_text_input-action"]["value"] = None
 
                     if button_select == "yes":
-                        value = generate_label_element("All")  
+                        value = generate_label_element("All")
                     else:
                         value = generate_label_element("None")
                     view_json["blocks"][index-1]["element"] = value
 
                 else:
-                    
-                    
                     print(condition)
                     update_button_json = generate_options_block(date)
                     view_json["blocks"][index-1]["element"] = generate_label_block()["element"]
-                
-                item["elements"] = update_button_json["elements"]
-                
-                    
 
+                item["elements"] = update_button_json["elements"]
                 break
-        
+
         body["view"]["state"]["values"][date]["plain_text_input-action"]["value"] = value
 
     ack()
 
     # モーダルを　更新する
     client.views_update(
-        view = view_json,
-        hash = body["view"]["hash"],
-        view_id = body["view"]["id"])
+        view=view_json,
+        hash=body["view"]["hash"],
+        view_id=body["view"]["id"])
 
 
 def update_option(body: dict) -> dict:
@@ -234,16 +231,15 @@ def update_option(body: dict) -> dict:
     target_date = body["actions"][0]["block_id"]
     temp = body["view"]["blocks"]
 
-    target_blocks = [ item for item in temp 
-        if "block_id" in item and target_date in item["block_id"] ]
+    target_blocks = [item for item in temp if "block_id" in item and target_date in item["block_id"]]
 
     option_num = len(target_blocks)
     option_time = str(target_blocks[0]["text"]["text"].split("の")[-1].strip(' *'))
 
     for i in range(len(temp)):
-        if target_date in temp[i]["block_id"] :
-                temp.insert(i+2,generate_time_block(target_date,option_time,option_num))
-                break
+        if target_date in temp[i]["block_id"]:
+            temp.insert(i+2, generate_time_block(target_date, option_time, option_num))
+            break
 
     return temp
 
@@ -251,8 +247,8 @@ def update_option(body: dict) -> dict:
 def update_time(body: dict) -> dict:
     """回答用 Modal の時間選択ブロックを更新する．"""
 
-    #TODO:　入力した時間が有効か確認する
-    #TODO:　重複しているか確認
+    # TODO:　入力した時間が有効か確認する
+    # TODO:　重複しているか確認
 
     return body["view"]["blocks"]
 
@@ -263,7 +259,7 @@ def get_modal_inputs(body: dict, values: dict) -> dict:
     member = body["user"]["id"]
     target = body["view"]["blocks"][-1]["elements"][0]["text"].split(':')[-1]
     secret = body["view"]["blocks"][-1]["elements"][1]["alt_text"]
-    dates,date = {}, "date"
+    dates, date = {}, "date"
 
     # 入力した日時を取得する
     """
@@ -280,7 +276,7 @@ def get_modal_inputs(body: dict, values: dict) -> dict:
             # TODO:終日用に設定する
             sets = []
         else:
-            sets = [ temp["member_start-timepicker-action"]["selected_time"], 
+            sets = [ temp["member_start-timepicker-action"]["selected_time"],
                      temp["member_end-timepicker-action"]["selected_time"] ]
 
         dates[date].append(sets)
@@ -291,10 +287,10 @@ def get_modal_inputs(body: dict, values: dict) -> dict:
         dates[item] = values[item]["plain_text_input-action"]["value"]
 
     return {
-        "target" : target,
-        "secret_value" : secret,
-        "available_date" : dates,
-        "member" : member
+        "target": target,
+        "secret_value": secret,
+        "available_date": dates,
+        "member": member
     }
 
 
@@ -305,18 +301,18 @@ def get_message(value: str, client: WebClient):
 
     # Target message
     message_list = client.conversations_history(
-        channel = target_channel,
-        oldest = post_time,
-        inclusive = True,
+        channel=target_channel,
+        oldest=post_time,
+        inclusive=True,
         limit=1)["messages"]
-    
+
     if message_list == []:
         return None
 
     message_info = message_list[0]
     target_message = message_info["ts"]
 
-    result = { "channel" : target_channel }
+    result = {"channel": target_channel}
 
     # スレッドの確認
     thread_present = False
@@ -324,15 +320,15 @@ def get_message(value: str, client: WebClient):
         thread_present = True
 
         reply_content = client.conversations_replies(
-            channel = target_channel,
-            ts = message_info["thread_ts"],
+            channel=target_channel,
+            ts=message_info["thread_ts"],
             )["messages"][-1]
 
         target_message = reply_content["ts"]
         message_content = reply_content["blocks"][0]["text"]["text"]
-        
+
         result["message"] = message_content
-    
+
     result["thread_present"] = thread_present
     result["channel"] = target_channel
     result["ts"] = target_message
@@ -344,27 +340,27 @@ def check_modal(ack: Ack, body: dict, client: WebClient, view: dict):
     """回答用 Modal の提出を確認する．"""
 
     values = view["state"]["values"]
-    inputs = get_modal_inputs(body,values)
+    inputs = get_modal_inputs(body, values)
     secret_value = inputs["secret_value"]
 
     ack()
-    
-    #　メッセージを送信する
-    send_answer(inputs,secret_value,client)
+
+    # メッセージを送信する
+    send_answer(inputs, secret_value, client)
 
 
 def send_answer(inputs: dict, secret_value: str, client: WebClient):
     """回答用 Modal の提出を確認メッセージを送信する．"""
 
-    #　スケジュール調整の詳細メッセージスレッドを取得
-    result = get_message(secret_value,client)
+    # スケジュール調整の詳細メッセージスレッドを取得
+    result = get_message(secret_value, client)
 
     message_json = read_json("./message/from_member-yes.json")
-    message_json[0]["text"]["text"] = message_json[0]["text"]["text"].replace("I",f"<@{inputs['member']}>")
+    message_json[0]["text"]["text"] = message_json[0]["text"]["text"].replace("I", f"<@{inputs['member']}>")
 
     if result["thread_present"]:
         message_json[0]["text"]["text"] += f"\n{result['message']}"
-    
+
     # 主催者にメッセージを送信する
     send_host(
         thread=result["thread_present"],
@@ -375,8 +371,8 @@ def send_answer(inputs: dict, secret_value: str, client: WebClient):
     )
 
     # モーダルの入力を表示する
-    print(f"\n\nモーダルの入力")
-    pprint({"result" : inputs['available_date']})
+    print("\n\nモーダルの入力")
+    pprint({"result": inputs['available_date']})
 
 
 def send_not_answer(ack: Ack, body: dict, client: WebClient):
@@ -384,16 +380,16 @@ def send_not_answer(ack: Ack, body: dict, client: WebClient):
 
     member = body["user"]["id"]
     secret_value = body["actions"][0]["value"]
-    
-    #　スケジュール調整の詳細メッセージスレッドを取得
+
+    # スケジュール調整の詳細メッセージスレッドを取得
     result = get_message(secret_value, client)
 
     message_json = read_json("./message/from_member-no.json")
-    message_json[0]["text"]["text"] = message_json[0]["text"]["text"].replace("I",f"<@{member}>")
-    
+    message_json[0]["text"]["text"] = message_json[0]["text"]["text"].replace("I", f"<@{member}>")
+
     if result["thread_present"]:
         message_json[0]["text"]["text"] += f"\n{result['message']}"
-    
+
     ack()
 
     # 主催者にメッセージを送信する
@@ -412,23 +408,23 @@ def send_host(thread: bool, target_channel: str, target_message: str, input_bloc
     # スレッドの確認
     if thread:
         client.chat_update(
-            channel = target_channel,
-            text = "メッセージを確認してください",
-            blocks = input_block,
-            ts = target_message,
-            as_user = True)
+            channel=target_channel,
+            text="メッセージを確認してください",
+            blocks=input_block,
+            ts=target_message,
+            as_user=True)
     else:
         client.chat_postMessage(
-            channel = target_channel,
-            text = "メッセージを確認してください",
-            blocks = input_block,
-            thread_ts = target_message,
-            as_user = True)
+            channel=target_channel,
+            text="メッセージを確認してください",
+            blocks=input_block,
+            thread_ts=target_message,
+            as_user=True)
 
 
 def register(app):
     logger.info("register")
-    
+
     # メッセージのクリック時
     app.action("answer_schedule")(open_modal)
     app.action("not_answer")(send_not_answer)
@@ -437,10 +433,10 @@ def register(app):
     app.action("member-add_date")(update_modal)
 
     # 時間の選択時
-    #app.action("member_start-timepicker-action")(update_modal)
-    #app.action("member_end-timepicker-action")(update_modal)
-    #app.action("member_check-action")(update_modal)
-    
+    # app.action("member_start-timepicker-action")(update_modal)
+    # app.action("member_end-timepicker-action")(update_modal)
+    # app.action("member_check-action")(update_modal)
+
     app.action("click_option-yes")(update_modal)
     app.action("click_option-no")(update_modal)
 
