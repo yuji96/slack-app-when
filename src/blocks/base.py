c93a2c318fbcf2dd3c7b03764d469cdc97d66d41
yuji96 <y.yama683@gmail.com>
2021-08-12 14:37:06 +0900

    modify text blocks

--- src/blocks/base.py
+++ src/blocks/base.py
@@ -1,15 +1,36 @@
+# TODO: 全体的に継承元を考え直す必要がある
+
 import json
 
 
-class Modal(dict):
-    def __init__(self, callback_id, title, submit, *args, **kwargs):
+class Text(dict):
+    TEXT_TYPE = None
+
+    def __init__(self, text, *args, **kwargs):
+        super().__init__(type=self.TEXT_TYPE, text=text, *args, **kwargs)
+
+
+class PlainText(Text):
+    TEXT_TYPE = "plain_text"
+
+
+class MarkDown(Text):
+    TEXT_TYPE = "mrkdwn"
+
+
+class Json(dict):
+    def __str__(self) -> str:
+        return json.dumps(self, indent=4)
+
+
+class Modal(Json):
+    def __init__(self, callback_id, title, submit, blocks=[], *args, **kwargs):
         super().__init__(type="modal", callback_id=callback_id,
-                         title=dict(type="plain_text", text=title),
-                         submit=dict(type="plain_text", text=submit),
-                         *args, **kwargs)
+                         title=PlainText(title), submit=PlainText(submit),
+                         blocks=blocks, *args, **kwargs)
 
 
-class Block(dict):
+class Block(Json):
     TYPE = None
 
     def __init__(self, block_id=None, accessory=None, *args, **kwargs):
@@ -18,9 +39,6 @@ class Block(dict):
             self["block_id"] = block_id
         if accessory:
             self["accessory"] = accessory
-
-    def __str__(self) -> str:
-        return json.dumps(self, indent=4)
 
 
 class Input(Block):
@@ -37,13 +55,6 @@ class Action(Block):
 
     def __init__(self, *elements):
         super().__init__(elements=list(elements))
-
-
-class Text(Block):  # TODO: いらない説
-    TYPE = None
-
-    def __init__(self, t_type, text, *args, **kwargs):
-        super().__init__(text=dict(type=t_type, text=text), *args, **kwargs)
 
 
 class Section(Text):
@@ -99,6 +110,9 @@ class PlainTextInput(Block):
 
 
 if __name__ == "__main__":
-    tmp = Input(block_id="block_id", label="label",
-                element=PlainTextInput(action_id="action_id", initial="inital", placeholder="text"))
+    # tmp = Input(block_id="block_id", label="label",
+    #             element=PlainTextInput(action_id="action_id",
+    #                                    initial="inital",
+    #                                    placeholder="text"))
+    tmp = Modal("id", "title", "submit", blocks=[])
     print(tmp)
